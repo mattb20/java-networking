1825e1c88c09530e561ae74aa683434877e17ca1
Matthew Burgoyne <matt_burgoyne@live.co.uk>
2020-05-15 11:27:23 +0100

    server + client

---
+++ src/main/client.java
@@ -0,0 +1,27 @@
+public class EchoClient {
+    private DatagramSocket socket;
+    private InetAddress address;
+
+    private byte[] buf;
+
+    public EchoClient() {
+        socket = new DatagramSocket();
+        address = InetAddress.getByName("localhost");
+    }
+
+    public String sendEcho(String msg) {
+        buf = msg.getBytes();
+        DatagramPacket packet
+          = new DatagramPacket(buf, buf.length, address, 4445);
+        socket.send(packet);
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+        String received = new String(
+          packet.getData(), 0, packet.getLength());
+        return received;
+    }
+
+    public void close() {
+        socket.close();
+    }
+}
